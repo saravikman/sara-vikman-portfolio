508d2551eba07343ea82ce28127d7da6955e27ee
saravikman <saravikman@hotmail.com>
2023-08-23 22:40:40 +0200

    Added cursor to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import Cursor from "./cursor"
+import AnimatedCursor from "react-animated-cursor"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -17,7 +18,16 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header theme="black"></Header>
-      <Cursor></Cursor>
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2.5}
+        color='11, 11, 11'
+        outerAlpha={0.1}
+        outerStyle={{
+          border: '0px solid #333'
+        }} />
       <div className="m-auto">
         <main>{children}</main>
         <footer className="m-auto mt-10 md:mt-36 py-8 text-center font-mono text-sm border-t border-zinc-200">
